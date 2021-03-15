a5e3102ea0321ce19bfe074b2b2c0e27d5e3182b
James Tan <jamestansx@gmail.com>
2021-03-15 19:58:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from modules import config, hdfbot, websetup
 
 
 def setup():
-    webdriverPath,matrixNumber, isFirstRun = config.isFirstRun()
+    webdriverPath, matrixNumber, isFirstRun = config.getSettings()
     return websetup.open_hdf(webdriverPath), matrixNumber, isFirstRun
+
 
 def main(driver, matrixNumber, isFirstRun):
     hdfbot.bot(driver, matrixNumber)
     if isFirstRun is True:
         config.updateSetting()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     driver, matrixNumber, isFirstRun = setup()
     main(driver, matrixNumber, isFirstRun)
