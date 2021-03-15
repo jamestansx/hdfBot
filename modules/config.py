dfb21a08d360210ccc3cfae9b4a5f885c4d0cbc1
James Tan <jamestansx@gmail.com>
2021-03-15 19:56:36 +0800

    Update config.py
    Add function to be able to setup the configuration

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
-import logging
 import os
+import sys
 
 import modules.jsonfile as jsonfile
 import modules.setting as setting
@@ -9,21 +9,51 @@ appname = "HDF_bot"
 
 
 def isFirstRun():
+    userData_json = get_userdata_dir()
+    response = None
+    if os.path.isfile(userData_json):
+        response = isEditSetting()
+    else:
+        setupSetting(userData_json)
+
+    if response:
+        editSetting(userData_json)
+    else:
+        sys.exit(1)
+
+
+def editSetting(userData_json):
+    data = jsonfile.read_json(userData_json)
+    choice = input("Select the setting to edit:\n1. chrome driver path\n2. matrix number\n[all]\n")
+    if choice in {"1", "all"}:
+        data["webdriverPath"] = input("Chrome driver path: ")
+    if choice in {"2", "all"}:
+        data["matrixNumber"] = input("Matrix number: ")
+    jsonfile.update_json(userData_json, data)
+
+
+def get_userdata_dir():
     dirs = setting.getDirs(appname, appauthor)
     userData_json = os.path.join(dirs["userData"], "userdata.json")
-    if os.path.isfile(userData_json):
-        return getSettings(userData_json)
-    else:
-        return setupSetting(userData_json)
+    return userData_json
+
+
+def isEditSetting():
+    while True:
+        response = input("Setting is already existed\nDo you want to edit <y/n>: ")
+        if response.strip().lower() in {"y", "yes"}:
+            return True
+        elif response.strip().lower() in {"n", "no"}:
+            return False
+        else:
+            pass
 
 
 def setupSetting(pathToFile):
     webdriverPath = input("Enter the path to Chrome driver: ")
-    logging.info("paths is entered")
     matrix_number = input("Enter your matrix number: ")
     data = {"webdriverPath": webdriverPath, "matrixNumber": matrix_number}
     jsonfile.write_json(pathToFile, writeSettings(data))
-    return webdriverPath, matrix_number, True
 
 
 def writeSettings(pathDict):
@@ -35,17 +65,14 @@ def writeSettings(pathDict):
     return data
 
 
-def getSettings(pathToFile):
-    data = jsonfile.read_json(pathToFile)
-    try:
-        if data["isFirstRun"] is True:
-            logging.warning("Setup is not yet been done with isFirstRun: " + data["isFirstRun"])
-            return setupSetting(pathToFile)
-    except Exception as e:
-        logging.error(e)
+def getSettings():
+    pathToFile = get_userdata_dir()
+    if os.path.isfile(pathToFile):
+        data = jsonfile.read_json(pathToFile)
+    else:
+        pass
     webdriverPath = data["webdriverPath"]
     matrixNumber = data["matrixNumber"]
-    logging.info("Path(s) are successfully extracted")
     return webdriverPath, matrixNumber, data["isFirstRun"]
 
 
