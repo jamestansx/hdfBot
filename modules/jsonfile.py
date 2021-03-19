75d3b658dd85783b34c839fdd7f6ca4bf82dec5d
James Tan <jamestansx@gmail.com>
2021-03-19 13:32:19 +0800

    Update saving json with indentation

--- modules/jsonfile.py
+++ modules/jsonfile.py
@@ -3,7 +3,7 @@ import json
 
 def write_json(pathToFile, data):
     with open(pathToFile, "w") as outFile:
-        json.dump(data, outFile)
+        json.dump(data, outFile, indent=2)
 
 
 def read_json(pathToFile):
